b550d4356a4969e936c1c559aa055acfd20e4e2e
carl.nikitin <carl.nikitin@khk.ee>
2018-12-13 13:06:46 +0200

    Väljastamine

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carl.nikitin
+ * Date: 13.12.2018
+ * Time: 12:46
+ */
+
+// teksti väljastamine
+// print
+print('Tere Maailm! <br>');
+print('PHP alused <br>');
+
+// echo
+echo 'See on echo <br>';
+
+// väärtuste ühendamine
+echo 'See'.' on'.' kolmest sõnast kokku pandud <br>';
