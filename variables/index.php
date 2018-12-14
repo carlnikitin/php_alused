e033a4069ac5c7d2569ee154bdb15e03ab46bd2d
carl.nikitin <carl.nikitin@khk.ee>
2018-12-14 12:32:09 +0200

    Matemaatilised- ja võrldusoperatoorid

--- variables/index.php
+++ variables/index.php
@@ -1,28 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: carl.nikitin
- * Date: 13.12.2018
- * Time: 13:08
- */
+// matemaatilised võrdlusoperaatorid
 
-//muutujate defineerimine
-$taisArv = 3;
-$reaalArv = 3.3;
-$sone = '3';
-$toevaartusTrue = true;
-$toevaartusFalse = false;
+$arv1 = 42;
+$arv2 = 11;
 
-//väljastame muutujate väärtused
-echo 'Täisarv '. $taisArv.'<br>';
-echo 'Reaalarv '. $realArv.'<br>';
-echo 'Sõne '. $sone.'<br>';
-echo 'Tõeväärtus true '. $toevaartusTrue.'<br>';
-echo 'Tõeväärtus false '. $toevaartusFalse.'<br>';
-echo '<hr>';
+echo $arv1.'+'.$arv2.'='.($arv1 + $arv2).'<br>';
+echo $arv1.'-'.$arv2.'='.($arv1 - $arv2).'<br>';
+echo $arv1.'*'.$arv2.'='.($arv1 * $arv2).'<br>';
+echo $arv1.'/'.$arv2.'='.($arv1 / $arv2).'<br>';
+echo $arv1.'%'.$arv2.'='.($arv1 % $arv2).'<br>';
 
-//tüübi kontroll
-echo gettype($sone);
-echo '<br>';
-$summa = $sone + $sone;
-echo 'Summa = '.$summa.'<br>';
+echo'<hr>';
+
+echo $arv1.' == '.$arv2.' on '.($arv1 == $arv2).'<br>';
+echo $arv1.' != '.$arv2.' on '.($arv1 != $arv2).'<br>';
+echo $arv1.' <= '.$arv2.' on '.($arv1 <= $arv2).'<br>';
+echo $arv1.' >= '.$arv2.' on '.($arv1 >= $arv2).'<br>';
+echo $arv1.' < '.$arv2.' on '.($arv1 < $arv2).'<br>';
+echo $arv1.' > '.$arv2.' on '.($arv1 > $arv2).'<br>';
+
