3597c36e4ca98bf1bde09cd7f9ebcccb290f41f0
carl.nikitin <carl.nikitin@khk.ee>
2018-12-17 15:10:07 +0200

    Argumentide andmine viite abil

--- functions/index.php
+++ functions/index.php
@@ -1,18 +1,10 @@
 <?php
 // funktsioonid
 
-$num_of_calls = 0;
-
-function andAnotherThing($txt) {
-    global $num_of_calls;
-    $num_of_calls++;
-    print("<h1>$num_of_calls. $txt</h1>");
+function addFive($num) {
+    $num += 5;
 }
-
-andAnotherThing("Widgets");
-print("<p>We build a fine range of widgets</p>");
-
-andAnotherThing("Doodads");
-print("<p>Finest in the world</p>");
-
+$orignum = 10;
+addFive($orignum);
+print($orignum);
 ?>
