7666bf9965338d309c12d1b4450e0b73f20612cd
carlnikitin <carl.nikitin@khk.ee>
2019-01-24 13:51:23 +0200

    lisatud võimalused andmebaasi andmete sisestamiseks

--- db/index.php
+++ db/index.php
@@ -8,3 +8,15 @@ $iktConn = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
 
 echo '<pre>';
 print_r($iktConn);
+//insert/update tüüpi sql testimine
+$sql = 'UPDATE user SET last_name = "Ebatavaline" WHERE user_id = 1';
+$res = query($sql, $iktConn);
+
+//select tüüpi sql testimine
+$sql = 'SELECT * FROM user';
+$res = getData($sql, $iktConn);
+
+echo '<pre>';
+print_r($res);
+
+echo 'Tere, '.$res[0]['first_name']. ' '.$res[0]['last_name'].'<br>';
