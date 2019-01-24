d01f70607bfcbd196cfb453de39a07267ab195c7
carlnikitin <carl.nikitin@khk.ee>
2019-01-24 14:34:49 +0200

    lisatud functionid ja action

--- session/fnk.php
+++ session/fnk.php
@@ -1,1 +1,10 @@
 <?php
+
+function htmlFromFile($fileName) {
+    $fileName = 'htmll/'.$fileName.'.html';
+    $fp = fopen($fileName, 'r');
+    $content = fread($fp, filesize($fileName));
+    fileclose($fp);
+
+    return $content;
+}
