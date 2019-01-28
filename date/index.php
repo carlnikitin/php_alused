7b374aae1e2db9d164d88ccce15ba4aa0f5913d9
carlnikitin <carl.nikitin@khk.ee>
2019-01-28 14:45:06 +0200

    Kuu kuvamine eesti keeles

--- date/index.php
+++ date/index.php
@@ -12,4 +12,16 @@ date_default_timezone_set('Europe/Tallinn');
 //Pika kuupäeva eestistamine
 echo date('d.F.Y');
 
+//Kuna kuu kuvatakse inglis keeles, muudame selle eesti keelseks
+//kuude massiiv
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+//kuupäevad massiividesse
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+//kuupäeva väljastamine
+echo $paev.'.'.$kuu.' '.$aasta;	//22.veebruar2013
+
+
+
 ?>
