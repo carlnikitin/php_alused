2507f1fc0fafd94556db760d4f2b1b6bf65241f2
carlnikitin <carl.nikitin@khk.ee>
2019-01-28 14:44:05 +0200

    Ajavöönd ja pika kuupäeva kuvamine

--- date/index.php
+++ date/index.php
@@ -6,6 +6,10 @@ echo time(); //1361551056
 //Kasutajasõbraliku kuupäeva kuvamine
 echo date('d.m.Y G:i' , time());
 
+//Ajavöönd
+date_default_timezone_set('Europe/Tallinn');
 
+//Pika kuupäeva eestistamine
+echo date('d.F.Y');
 
 ?>
